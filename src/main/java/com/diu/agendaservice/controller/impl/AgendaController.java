fa074bd70768fd62f43364e2e24533f32823f710
daaniigomez29 <danigb2904@gmail.com>
2024-02-09 11:06:33 +0100

    Mapping añadido

--- src/main/java/com/diu/agendaservice/controller/impl/AgendaController.java
+++ src/main/java/com/diu/agendaservice/controller/impl/AgendaController.java
@@ -4,39 +4,45 @@ import com.diu.agendaservice.controller.AgendaAPI;
 import com.diu.agendaservice.entity.Person;
 import com.diu.agendaservice.service.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1")
 public class AgendaController implements AgendaAPI {
 
     @Autowired
     private AgendaService agendaService;
 
     @Override
+    @GetMapping("/person")
     public List<Person> obtainListPerson() {
         return agendaService.obtainListPerson();
     }
 
     @Override
-    public Optional<Person> obtainPerson(String id) {
+    @GetMapping("/person/{id}")
+    public Optional<Person> obtainPerson(@PathVariable String id) {
         return agendaService.obtainPerson(id);
     }
 
     @Override
-    public Person addPerson(Person p) {
+    @PostMapping("/person")
+    public Person addPerson(@RequestBody Person p) {
         return agendaService.addPerson(p);
     }
 
     @Override
-    public Person editPerson(Person p) {
+    @PutMapping("/person/{id}")
+    public Person editPerson(@RequestBody Person p) {
         return agendaService.editPerson(p);
     }
 
     @Override
-    public void deletePerson(String id) {
+    @DeleteMapping("/person/{id}")
+    public void deletePerson(@PathVariable String id) {
         agendaService.deletePerson(id);
     }
 }
